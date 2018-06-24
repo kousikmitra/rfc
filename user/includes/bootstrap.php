81ace841c2b9a18e9aa6a1136750f7acf1fc21f8
Sagar Samanta <sagarsamanta390@gmail.com>
2018-06-24 13:01:11 -0400

    home done

---
+++ user/includes/bootstrap.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" href="..\vendor\bootstrap-4.0.0-dist\css\bootstrap.min.css">
+<script src="..\vendor\bootstrap-4.0.0-dist\js\bootstrap.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" rel="stylesheet"/>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<script src="..\vendor\bootstrap-4.0.0-dist\js\bootstrap.min.js"></script>
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+ <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js" integrity="sha384-u/bQvRA/1bobcXlcEYpsEdFVK/vJs3+T+nXLsBYJthmdBuavHvAW6UsmqO2Gd/F9" crossorigin="anonymous"></script>
+ 
--- user/index.php
+++ user/index.php
@@ -29,83 +29,171 @@ if(isset($_POST['login'])){
 
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="./css/index.css">
-    <link rel="stylesheet" href="../vendor/bootstrap-4.0.0-dist/css/bootstrap.min.css">
-    <title>Rail Food Login</title>
-</head>
-<body>
-    <div class="main">
-        <div class="top-bar">
-            
-     <nav class="navbar navbar-expand-lg navbar-light bg-light rounded">
-     <a class="navbar-brand" href="#">
-     <div class="title">
-                <img src="./images/logo.png" alt="">
-            </div>
-    </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+    <!DOCTYPE html>
+    <html lang="en">
 
-        <div class="collapse navbar-collapse">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#">Disabled</a>
-            </li>
-          </ul>
-          
-        </div>
-      </nav>
-        </div>
-        
-        <div class="main-content">
-            <div class="login-section">
-                <div class="head-text">
-                    <h1>Get food delivered on train berth</h1>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <?php include_once "./includes/bootstrap.php"; ?>
+        <link rel="stylesheet" href="./css/index.css">
+        <title>Rail Food Login</title>
+    </head>
+
+    <body>
+        <div class="main">
+            <div class="top-bar">
+                <div class="logo">
+                    <img src="./images/logo.png" alt="">
                 </div>
-            <div class="login">
-                <form action="" method="post" class="login-form">
-                   <fieldset>
-                   <legend>Login</legend>
-                   <?php
+                <div class="menu">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav mr-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#">Home
+                                        <span class="sr-only">(current)</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Link</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Disabled</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Disabled</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Disabled</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Disabled</a>
+                                </li>
+                            </ul>
+
+                        </div>
+                    </nav>
+                </div>
+            </div>
+
+            <div class="main-content">
+                <div class="login-section">
+                    <div class="head-text">
+                        <h1>Get food delivered on train berth</h1>
+                    </div>
+                    <div class="login">
+                        <form action="" method="post" class="login-form">
+                            <fieldset>
+                                <legend>Login</legend>
+                                <?php
                    if($_SESSION['err_msg'] != ""){
                    ?>
-                   <div class="alert alert-danger">
-                    <strong>Failed!</strong> <?php echo $_SESSION['err_msg']; ?>
-                    </div>
-                    <?php
+                                    <div class="alert alert-danger">
+                                        <strong>Failed!</strong>
+                                        <?php echo $_SESSION['err_msg']; ?>
+                                    </div>
+                                    <?php
                    }
                    ?>
-                   <div class="form-group">
-                    <label for="email">Email :</label>
-                    <input type="email" name="email" id="email" class="form-control" required>
+                                        <div class="form-group">
+                                            <label for="email">Email :</label>
+                                            <input type="email" name="email" id="email" class="form-control" required>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="password">Password :</label>
+                                            <input type="password" name="password" id="password" class="form-control" required>
+                                        </div>
+                                        <input type="submit" value="login" name="login" id="login" class="btn btn-primary">
+                                        <br>
+                                        <br>
+                                        <a href="#">forgot password?</a>
+                                        <br>
+                                        <a href="./signup.php">Sign Up!</a>
+                            </fieldset>
+                        </form>
                     </div>
-                    <div class="form-group">
-                    <label for="password">Password :</label>
-                    <input type="password" name="password" id="password" class="form-control" required>
+                </div>
+                <div class="mid">
+                    <h1>Order Food In Simple Steps</h1>
+                    <div class="order-step">
+                        <div>
+                            <img src="https://www.railrestro.com/img/step1.gif " class="stepimages" title="#" alt="Favorite Food - Rail restro" rel="nofollow">
+                            <p class="step">
+                                <b>Enter PNR or Train Number.</b>
+                            </p>
+                        </div>
+                        <div>
+                            <img src="https://www.railrestro.com/img/step2.png " class="stepimages" title="#" alt="Favorite Food - Rail restro" rel="nofollow">
+                            <p class="step">
+                                <b>Choose your favourite food.</b>
+                            </p>
+                        </div>
+                        <div>
+                            <img src="https://www.railrestro.com/img/step3.png " class="stepimages" title="#" alt="Favorite Food - Rail restro" rel="nofollow">
+                            <p class="step">
+                                <b>Pay online or cash on delivery.</b>
+                            </p>
+                        </div>
+                        <div>
+                            <img src="https://www.railrestro.com/img/step4.jpg " class="stepimages" title="#" alt="Favorite Food - Rail restro" rel="nofollow">
+                            <p class="step">
+                                <b>Enjoy your food at your Seat.</b>
+                            </p>
+                        </div>
                     </div>
-                    <input type="submit" value="login" name="login" id="login" class="btn btn-primary"><br><br>
-                    <a href="#">forgot password?</a><br>
-                    <a href="./signup.php">Sign Up!</a>
-                   </fieldset>
-                </form>
-            </div>
-            </div>
+                </div>
 
+                <div class="mid2">
+                    <div class="slide-show" style="height:350px; width: 600px; margin-top:50px;">
+                        <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                            </ol>
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img class="d-block w-100" src="./images/food 1.jpg" style="height:350px; width: 400px;" alt="First slide">
+
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="./images/food 2.jpg" style="height:350px; width: 400px;" alt="Second slide">
+
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="./images/food 3.jpg" style="height:350px; width: 400px;" alt="Third slide">
+
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="./images/food 4.jpg" style="height:350px; width: 400px;" alt="Third slide">
+
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100" src="./images/logo 6.jpg" style="height:350px; width: 400px;" alt="Third slide">
+
+                                </div>
+                            </div>
+                            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-    <script src="../vendor/bootstrap-4.0.0-dist/js/bootstrap.min.js"></script>
-</body>
-</html>
+
+    </body>
+
+    </html>
