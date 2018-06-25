6741def41e0db1ddc342c351d1ebb50a6acc90a1
Sagar Samanta <sagarsamanta390@gmail.com>
2018-06-25 17:57:56 -0400

    food.php done

--- user/includes/bootstrap.php
+++ user/includes/bootstrap.php
@@ -10,4 +10,6 @@
  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js" integrity="sha384-u/bQvRA/1bobcXlcEYpsEdFVK/vJs3+T+nXLsBYJthmdBuavHvAW6UsmqO2Gd/F9" crossorigin="anonymous"></script>
+ <link rel="stylesheet" href="..\vendor\bootstrap-datepicker\bootstrap-datepicker.min.js">
+ <script src="..\vendor\bootstrap-datepicker\bootstrap-datepicker.min.js"></script>
  
--- user/includes/item.php
+++ user/includes/item.php
@@ -1,5 +1,53 @@
 <div class="col-md-5 col-xs-12">
     <div class="item">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae odit dolorum blanditiis ducimus nostrum veniam minus, culpa, numquam tenetur corrupti facere at repellat, perferendis expedita aliquam iure? Veniam, quaerat voluptatem!
+        <div class="head"></div>
+        <div class="main-item">
+            <div class="food-image">
+                <div style="padding:4px;border:0.5px #b1adadaf solid; border-radius:2%; width: auto;">
+                    <img src="./images/food 1.jpg" alt="" height=150 width=150>
+                </div>
+            </div>
+            <div class="food-detail">
+                <h4 class="food-name">Butter Paneer Masala</h4>
+                <div class="price">
+                    <p>
+                        <i class="fa fa-rupee"></i> 135.00</p>
+                </div>
+                <table class="rating">
+                    <tr>
+                        <td>
+                            <div>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star checked"></span>
+                                <span class="fa fa-star-o"></span>
+                                <span class="fa fa-star-o"></span>
+                            </div>
+                        </td>
+                        <td>
+                            <small style="font-size:12px;"> &nbsp;&nbsp;Rating 2.81 / 5 | Reviews 449</small>
+                        </td>
+                    </tr>
+                </table>
+                <p>
+                    <small> MIN-ORDER
+                        <strong>199</strong>
+                        <span class="text-danger">
+                            <span class="fa fa-clock-o" title="Order Before Minutes"></span> 60 MINUTES</span>
+                    </small>
+                </p>
+                <div>
+                <div class="icons">
+                <img src="https://www.railrestro.com/img/icons/1.png" title="Veg Food">
+                <img src="https://www.railrestro.com/img/icons/2.png" title="Veg Food">
+                <img src="https://www.railrestro.com/img/icons/3.jpg" title="Veg Food">
+                <img src="https://www.railrestro.com/img/icons/5.png" title="Veg Food">
+                </div>
+                <div class="order">
+                <a href="" class="btn btn-success"><span class="fa fa-cutlery"></span> Order Food</a>
+                </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
